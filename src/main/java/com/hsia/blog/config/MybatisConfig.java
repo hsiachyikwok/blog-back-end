65b389ee84f2c8959d3af6e6fd1e21b4bc8d003d
hisachyikwok <hsiachyikwok@gmail.com>
2018-01-23 23:50:58 +0800

    配置swagger

---
+++ src/main/java/com/hsia/blog/config/MybatisConfig.java
@@ -0,0 +1,20 @@
+package com.hsia.blog.config;
+
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisConfig {
+//    @Bean
+//    ConfigurationCustomizer mybatisConfigurationCustomizer() {
+//        return new ConfigurationCustomizer() {
+//
+//            @Override
+//            public void customize(org.apache.ibatis.session.Configuration configuration) {
+//                //configuration.addCache();
+//
+//            }
+//        };
+//    }
+}
