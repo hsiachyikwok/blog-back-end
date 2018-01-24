e25fb43ae66fac5eddf2e0f50f72a3c1bdeeb5f6
hisachyikwok <hsiachyikwok@gmail.com>
2018-01-24 16:19:45 +0800

    创建dao mapper接口使用mybatis注解开发

---
+++ src/main/java/com/hsia/blog/dao/NavigationMapper.java
@@ -0,0 +1,12 @@
+package com.hsia.blog.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author: hsia
+ * @Date: 2018/1/24 下午4:17
+ * @Description:
+ */
+@Mapper
+public interface NavigationMapper {
+}
