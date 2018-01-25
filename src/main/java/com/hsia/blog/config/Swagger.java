0574af7212ba7cb27ba7a58f676b0d9c2f1ef489
hisachyikwok <hsiachyikwok@gmail.com>
2018-01-25 14:11:09 +0800

    修改swagger配置

--- src/main/java/com/hsia/blog/config/Swagger.java
+++ src/main/java/com/hsia/blog/config/Swagger.java
@@ -7,12 +7,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static com.google.common.base.Predicates.not;
 import static com.sun.tools.doclint.Entity.or;
+import static com.sun.tools.doclint.Entity.reg;
 import static springfox.documentation.builders.PathSelectors.regex;
 import static springfox.documentation.builders.RequestHandlerSelectors.withClassAnnotation;
 
@@ -20,18 +22,29 @@ import static springfox.documentation.builders.RequestHandlerSelectors.withClass
 @EnableSwagger2
 public class Swagger {
 
-    private static final String title = "swagger在线文档";
+    private static final String title = "online doc";
     private static final String description = "blog system";
     private static final String termsOfServiceUrl = "www.xiaqiguo.com";
-    private static final String contact = "xiaqiguo";
+    private static final String name = "hsiachyikwok";
+    private static final String url = "http://www.xiaqiguo.com";
+    private static final String email = "hsiachyikwok@gmail.com";
     private static final String version = "1.0";
+
+    @Bean
+    public Contact createContact(){
+        return new Contact(name,url,email);
+    }
 
     private ApiInfo apiInfo() {
 
         return new ApiInfoBuilder()
                 .title(title)
                 .description(description)
-                .version("1.0")
+                .termsOfServiceUrl(termsOfServiceUrl)
+                .contact(createContact())
+                .license("MIT")
+                .licenseUrl(url)
+                .version(version)
                 .build();
     }
 
