adde0fbf831f181f30f2bcb00f15bc9aa4708bcf
hisachyikwok <hsiachyikwok@gmail.com>
2018-01-25 10:56:57 +0800

    token拦截器

--- src/main/java/com/hsia/blog/interceptor/TokenInterceptor.java
+++ src/main/java/com/hsia/blog/interceptor/TokenInterceptor.java
@@ -1,9 +1,31 @@
 package com.hsia.blog.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author: hsia
  * @Date: 2018/1/24 下午9:02
  * @Description: token拦截器
  */
-public class TokenInterceptor {
+@Slf4j
+public class TokenInterceptor implements HandlerInterceptor{
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        log.info("进入token拦截器");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        log.info("进入postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        log.info("离开token拦截器");
+    }
 }
