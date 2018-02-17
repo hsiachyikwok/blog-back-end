3a8ed87bdb3c21ac494aac8415323a26be60d0cf
hisachyikwok <hsiachyikwok@gmail.com>
2018-02-17 22:44:05 +0800

    文件建好

---
+++ src/main/java/com/hsia/blog/service/CategoryServiceImpl.java
@@ -0,0 +1,14 @@
+package com.hsia.blog.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: hsia
+ * @Date: 2018/2/17 下午10:43
+ * @Description:
+ */
+@Service
+@Slf4j
+public class CategoryServiceImpl {
+}
